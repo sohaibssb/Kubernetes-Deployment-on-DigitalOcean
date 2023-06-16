1bcad797b3c84af946323a5249d55c242f2b5916
sohaibssb <sohayb.salem2012@gmail.com>
2023-06-16 20:47:25 +0300

    31

--- Microservers-CarRentalSystem/src/cars_service/blueprints/models/base_model.py
+++ Microservers-CarRentalSystem/src/cars_service/blueprints/models/base_model.py
@@ -1,39 +1,15 @@
-
 import os
-from dotenv import load_dotenv
 from peewee import Model, PostgresqlDatabase
 
-# Load environment variables from .env file
-load_dotenv()
-
-# Get database connection details from environment variables
-db_name = os.getenv('DATA_BASE_NAME')
-db_user = os.getenv('DATA_BASE_USER')
-db_pass = os.getenv('DATA_BASE_PASS')
-db_host = os.getenv('DATA_BASE_HOST')
-db_port = int(os.getenv('DATA_BASE_PORT'))
-
-# Create a PostgresqlDatabase instance
-pg_db = PostgresqlDatabase(db_name, user=db_user, password=db_pass, host=db_host, port=db_port)
+pg_db = PostgresqlDatabase(
+    "cars",
+    user="program",
+    password="test",
+    host="postgres",
+    port=5432
+)
+# port=int(os.getenv('DATA_BASE_PORT'))
 
 class BaseModel(Model):
     class Meta:
         database = pg_db
-
-
-
-# import os
-# from peewee import Model, PostgresqlDatabase
-
-# pg_db = PostgresqlDatabase(
-#     os.getenv('DATA_BASE_NAME'),
-#     user=os.getenv('DATA_BASE_USER'),
-#     password=os.getenv('DATA_BASE_PASS'),
-#     host=os.getenv('DATA_BASE_HOST'),
-#     port=int(os.getenv('DATA_BASE_PORT'))
-# )
-# # port=int(os.getenv('DATA_BASE_PORT'))
-
-# class BaseModel(Model):
-#     class Meta:
-#         database = pg_db
--- Microservers-CarRentalSystem/src/payment_service/blueprints/models/base_model.py
+++ Microservers-CarRentalSystem/src/payment_service/blueprints/models/base_model.py
@@ -3,11 +3,11 @@ from peewee import Model, PostgresqlDatabase
 
 
 pg_db = PostgresqlDatabase(
-    os.getenv('DATA_BASE_NAME'),
-    user=os.getenv('DATA_BASE_USER'),
-    password=os.getenv('DATA_BASE_PASS'),
-    host=os.getenv('DATA_BASE_HOST'),
-    port=int(os.getenv('DATA_BASE_PORT'))
+    "rentals",
+    user="program",
+    password="test",
+    host="postgres",
+    port=5432
 )
 
 
