a925b28bd3683bc8327a9ec7859731598b46a640
sohaibssb <sohayb.salem2012@gmail.com>
2023-06-17 10:49:26 +0300

    51

--- Microservers-CarRentalSystem/src/Authentication/src/config.js
+++ Microservers-CarRentalSystem/src/Authentication/src/config.js
@@ -2,9 +2,9 @@ import { OktaAuth } from "@okta/okta-auth-js";
 
 //constants use for okta login
 const oktaAuth = new OktaAuth({
-  issuer: `https://${process.env.REACT_APP_OKTA_BASE_URL}/oauth2/default`,
-  clientId: `${process.env.REACT_APP_OKTA_CLIENT_ID}`,
-  redirectUri: `${process.env.REACT_APP_OKTA_REDIRECTURI}` + "/login/callback",
+  issuer: "https://dev-26551516.okta.com/oauth2/default",
+  clientId: "0oa9vs52rl8RkBBPR5d7",
+  redirectUri: "http://0.0.0.0:3000/login/callback",
   scopes: ["openid" ,"offline_access", "email", "profile" ,],
   pkce: true,
 });
